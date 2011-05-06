beb1aa57f16e14a74799a2603f278f63187c3e58
hazz <get.off.my.suit@googlemail.com>
2011-05-07 00:01:34 +0100

    Initial Commit

---
+++ whist_cli/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  whist_cli
+//
+//  Created by Harry Maclean on 07/05/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
